25ef53d5ce6b3ec4beea0b845f91a48ca962466e
wadaka <erou1227@yahoo.co.jp>
2022-06-01 13:06:50 +0900

    コミット

---
+++ WebContent/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>どこつぶ</title>
+</head>
+<body>
+<h1>どこつぶへようこそ！</h1>
+<form action="/docoTsubu/Login" method="post">
+ユーザー名<input type="text" name="userName"><br>
+パスワード<input type="text" name="pass"><br>
+<p></p>
+<button type="submit">ログイン</button>
+</form>
+</body>
+</html>
---
+++ src/servlet/Login.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.LoginLogic;
+import model.User;
+
+/**
+ * Servlet implementation class Login
+ */
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String userName = request.getParameter("userName");
+		String pass = request.getParameter("pass");
+		User user = new User(userName,pass);
+		LoginLogic loginLogic = new LoginLogic();
+		boolean isLogin = loginLogic.execute(user);
+		if(isLogin) {
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", user);
+		}
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/loginResult.jsp");
+		rd.forward(request, response);
+	}
+
+}
